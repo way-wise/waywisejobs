7d578b3b47c53422d62944a9c075fae8a8d85ea6
naheantarik <naheantarik@gmail.com>
2024-12-03 14:14:00 +0600

    Footer customized

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -9,8 +9,8 @@ const Footer = () => {
     <footer className="bg-[#2A37EA] mt-[100px]">
       <div className="container mx-auto">
         <div className="pt-[40px] pb-[15px] md:flex justify-between">
-          <div className='flex flex-col gap-4 items-center pt-[30px]'>
-            <Image src={Logo} alt="logo" width={220} height={120}/>
+          <div className="flex flex-col gap-4 items-center pt-[30px]">
+            <Image src={Logo} alt="logo" width={220} height={120} />
             <p className="text-[16px] text-white">
               Don’t hesitate, contact us for better help and services.
             </p>
@@ -19,16 +19,16 @@ const Footer = () => {
             <h2 className="text-[20px] font-bold uppercase leading-[28px] mb-[20px]">
               QUICK LINKS
             </h2>
-            <ul className='font-bold'>
+            <ul className="font-bold">
               <li className=" text-[16px] leading-[26px] pb-[12px]">
-                <a href="#" className="hover:text-[#2A37EA] transition-colors">
+                <a href="#" className="hover:text-white/90 transition-colors">
                   Home
                 </a>
               </li>
               <li className="text-[16px] leading-[26px] pb-[12px]">
                 <a
                   href="#about-us"
-                  className="hover:text-[#2A37EA] transition-colors"
+                  className="hover:text-white/90 transition-colors"
                 >
                   About Us
                 </a>
@@ -36,7 +36,7 @@ const Footer = () => {
               <li className="text-[16px] leading-[26px] pb-[12px]">
                 <a
                   href="#countries-we-export"
-                  className="hover:text-[#2A37EA] transition-colors"
+                  className="hover:text-white/90 transition-colors"
                 >
                   Countries We Export
                 </a>
@@ -44,7 +44,7 @@ const Footer = () => {
               <li className="text-[16px] leading-[26px] pb-[12px]">
                 <a
                   href="#factories-we-work"
-                  className="hover:text-[#2A37EA] transition-colors"
+                  className="hover:text-white/90 transition-colors"
                 >
                   Factories We Work
                 </a>
@@ -52,7 +52,7 @@ const Footer = () => {
               <li className="text-[16px] leading-[26px] pb-[12px]">
                 <a
                   href="#contact-us"
-                  className="hover:text-[#2A37EA] transition-colors"
+                  className="hover:text-white/90 transition-colors"
                 >
                   Contact Us
                 </a>
@@ -105,15 +105,22 @@ const Footer = () => {
           </div>
         </div>
         <div className="border-t border-gray-700 mt-10">
-          <div className="container md:py-6 py-[20px] md:flex md:justify-between justify-center items-center text-center">
+          <div className="container md:py-6 py-[20px] md:flex md:justify-between justify-center items-center text-center group">
             <p className="text-gray-400 text-sm">
-              © 2024 Copyright Way-Wise Jobs. All rights reserved.
+              © 2024 Copyright{" "}
+              <span className="group-hover:text-[#FF902E] font-semibold">
+                Way-Wise Jobs.
+              </span>{" "}
+              All rights reserved.
             </p>
 
             <div className="flex items-center gap-6 justify-center">
               <div className="flex gap-4 text-sm text-gray-400 text-center">
                 <p>Developed by</p>
-                <a href="#" className="hover:text-[#FF902E] text-white font-bold transition-colors relative after:absolute after:bottom-0 after:left-0 after:w-0 after:h-2 after:bg-white">
+                <a
+                  href="#"
+                  className="hover:text-[#FF902E] text-white font-bold transition-colors relative after:absolute after:bottom-0 after:left-0 after:w-0 after:h-[2px] after:bg-[#FF902E] after:transition-all after:duration-300 hover:after:w-full"
+                >
                   Way-Wise Tech
                 </a>
               </div>
