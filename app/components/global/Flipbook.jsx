ae146541ca17a1c2caa467e7296849e8e7304769
WayWise Tech <waywisetechdevelopers@gmail.com>
2025-03-02 14:09:34 +0600

    Update Flipbook.jsx

--- app/components/global/Flipbook.jsx
+++ app/components/global/Flipbook.jsx
@@ -45,8 +45,12 @@ const Flipbook = () => {
               {/* Flipbook Content */}
               <div className="overflow-auto h-full">
                 {/* Replace with your flipbook component */}
+                {/*
+                // Replace with your flipbook component
+                // old link https://heyzine.com/flip-book/22813621d6.html
+                */}
                 <iframe
-                  src="https://heyzine.com/flip-book/22813621d6.html"
+                  src="https://heyzine.com/flip-book/5c587f0281.html"
                   className="w-full h-full border-none"
                   title="Flipbook"
                 />
